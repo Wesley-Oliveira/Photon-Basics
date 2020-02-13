55b4fcf559e77e0d9bbcdd7c43d0917a40ad6bce
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-13 11:19:35 -0300

    Custom room properties, custom lobby properties and list rooms

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 
+using Photon.Pun.UtilityScripts;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
 public class NetworkController : MonoBehaviourPunCallbacks
 {
     [Header("GO")]
@@ -18,6 +21,10 @@ public class NetworkController : MonoBehaviourPunCallbacks
 
     [Header("ROOM")]
     public InputField roomName;
+
+    Hashtable gameMode = new Hashtable(); //Esse gameMode vai possuir diversos valores de configuração de sala e configurações do modo de jogo
+    public byte gameMaxPlayer = 2; //Número max de players
+    string gameModeKey = "gameMode"; // A chave para facilitar quando quisermos apontar para o hashtable gameMode
 
     //Conectando aos servidores da Photon usando as configurações fornecidas no projeto
     void Start ()
@@ -48,16 +55,58 @@ public class NetworkController : MonoBehaviourPunCallbacks
         loginGO.SetActive(false);
     }
 
-    public void BotaoBuscarPartidaRapida()
-    {
-        PhotonNetwork.JoinLobby();
-    }
-
     public void BotaoCriarSala()
     {
         string roomNameTemp = roomName.text;
         RoomOptions roomOptions = new RoomOptions() { MaxPlayers = 4 };
         PhotonNetwork.JoinOrCreateRoom(roomNameTemp, roomOptions, TypedLobby.Default);
+    }
+
+    public void BotaoBuscarPartidaRapida()
+    {
+        //Buscar partida aleatória
+        string[] typeGameRandom = new string[]
+        {
+            "PvP",
+            "PvE"
+        };
+
+        gameMode.Add(gameModeKey, typeGameRandom[Random.Range(0, typeGameRandom.Length)]);
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void BotaoPartidaPvP()
+    {
+        gameMode.Add(gameModeKey, "PvP"); //Para adicionar o modo de jogo dentro do hashtable
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void BotaoPartidaPvE()
+    {
+        gameMode.Add(gameModeKey, "PvE"); //Para adicionar o modo de jogo dentro do hashtable
+        PhotonNetwork.JoinLobby();
+    }
+
+    // Listar as salas
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        ListasDeSalas(roomList);
+    }
+
+    void ListasDeSalas(List<RoomInfo> roomList)
+    {
+        foreach (var item in roomList)
+        {
+            Debug.Log("Room Name: " + item.Name);
+            Debug.Log("Room IsOpen: " + item.IsOpen);
+            Debug.Log("Room IsVisible: " + item.IsVisible);
+            Debug.Log("Room MaxPlayers: " + item.MaxPlayers);
+            Debug.Log("Room PlayerCount: " + item.PlayerCount);
+
+            object temp;
+            item.CustomProperties.TryGetValue(gameModeKey, out temp);
+            Debug.Log("Room CustomProperties: " + temp.ToString());
+        }
     }
 
     #region PunCallbacks
@@ -79,17 +128,23 @@ public class NetworkController : MonoBehaviourPunCallbacks
         //PhotonNetwork.JoinLobby();
     }
 
-    //Conectando a uma sala do lobby
+    //Conectando a uma sala do lobby passando como parâmetro a váriavel gameMode - onde possui as informações do modo de jogo
     public override void OnJoinedLobby()
     {
         Debug.Log("OnJoinedLobby");
-        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinRandomRoom(gameMode, gameMaxPlayer);
     }
 
-    //Caso ocorra falha ao conectar a uma sala, criar uma nova sala
+    //Caso ocorra falha ao conectar a uma sala, criar uma nova sala com características específicas definidas no hashtable gameMode
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        PhotonNetwork.CreateRoom("Room-" + Random.Range(100, 10000));
+        RoomOptions options = new RoomOptions();
+        options.IsOpen = true;
+        options.IsVisible = true;
+        options.MaxPlayers = gameMaxPlayer;
+        options.CustomRoomProperties = gameMode;
+        options.CustomRoomPropertiesForLobby = new string[] { gameModeKey };
+        PhotonNetwork.CreateRoom("Room-" + Random.Range(100, 10000), options);
     }
 
     //Caso entre em uma sala
@@ -101,6 +156,12 @@ public class NetworkController : MonoBehaviourPunCallbacks
 
         loginGO.SetActive(false);
         partidasGO.SetActive(false);
+
+        object typeGameValue;
+        if(PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(gameModeKey, out typeGameValue))
+        {
+            Debug.Log("GameMode: " + typeGameValue.ToString());
+        }
 
         //Para instanciar um objeto multiplayer
         PhotonNetwork.Instantiate(myPlayer.name, myPlayer.transform.position, myPlayer.transform.rotation, 0);
